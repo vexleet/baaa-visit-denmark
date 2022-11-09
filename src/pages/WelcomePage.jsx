57293b2318e4ceadc43ecbdc963da81a4b6a23bd
Katerina Kolarova <89975146+Poc-a-hontas@users.noreply.github.com>
2022-11-09 14:41:13 +0100

    adding img

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,7 +1,21 @@
 import { Typography } from '@mui/material';
 
 const WelcomePage = () => {
-  return <Typography variant="h1">welcome page</Typography>;
-};
+  return (
+    <>
+  <img
+        src={`logo.svg`}
+        alt=""
+  ></img>
+  <Typography>Welcome</Typography>
+  <Typography>Denmark has everything you love; beautiful nature, exciting culture and wild activities. Denmark has it all 
+    - the hard part is choosing what to experience. Plan out your day in Aarhurs here!
+  </Typography>
 
+    </>
+
+  )};
 export default WelcomePage;
+
+
+
