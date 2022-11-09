0f7e95d198530f7c048cc04dc29e5dc90eba8b85
Katerina Kolarova <89975146+Poc-a-hontas@users.noreply.github.com>
2022-11-09 18:24:22 +0100

    Update Breakfast.jsx

--- src/pages/Breakfast.jsx
+++ src/pages/Breakfast.jsx
@@ -2,16 +2,18 @@ import { Typography, Button } from '@mui/material';
 
 const Breakfast = () => {
   return (
-    <div>
-    <Typography style={{fontSize:"2.3rem"}}>How would you like your breakfast?</Typography>
+    <div style={{display:"flex", justifyContent:"center", flexDirection:"column"}}>
+    <Typography style={{fontSize:"1.5rem", marginTop:"-10rem", padding:"3rem"}}>How would you like your breakfast?</Typography>
 
-    <Button style={{backgroundColor:"white", color:"#022B52"}} variant="contained">I love early breakfast</Button>
+    <Button style={{alignContent:"center", alignSelf:"center", backgroundColor:"white", color:"#022B52", width:"17rem", height:"4rem", marginTop:"2rem"}} variant="contained">I love early breakfast</Button>
 
-    <Button style={{backgroundColor:"white", color:"#022B52"}} variant="contained">I prefer brunch</Button>
+    <Button style={{alignContent:"center", alignSelf:"center",backgroundColor:"white", color:"#022B52", width:"17rem", height:"4rem", marginTop:"1.5rem"}} variant="contained">I prefer brunch</Button>
 
-    <Typography>or</Typography>
+    <Typography style={{alignContent:"center", alignSelf:"center", marginTop:"1rem"}}>or</Typography>
 
-    <Button style={{backgroundColor:"white", color:"#022B52"}} variant="contained">No breakfast</Button>
+    <Button style={{alignContent:"center", alignSelf:"center", backgroundColor:"white", color:"#022B52", width:"17rem", height:"4rem"}} variant="contained">No breakfast</Button>
+    
+    <Button>Next</Button>
     </div>
   )
 };
