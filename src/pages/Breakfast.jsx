d0d9dbdfb0cc418c5705cc24f4136992f356797a
Katerina Kolarova <89975146+Poc-a-hontas@users.noreply.github.com>
2022-11-10 14:13:56 +0100

    background img, linking pages

--- src/pages/Breakfast.jsx
+++ src/pages/Breakfast.jsx
@@ -48,20 +48,12 @@ function Breakfast() {
         or
       </Typography>
 
-      <Button
-        style={{
-          alignContent: 'center',
-          alignSelf: 'center',
-          backgroundColor: 'white',
-          color: '#022B52',
-          width: '17rem',
-          height: '4rem'
-        }}
-        variant="contained">
-        No breakfast
-      </Button>
+    <Typography style={{alignContent:"center", alignSelf:"center", marginTop:"1rem", color:"white"}}>or</Typography>
 
-      <Button>Next</Button>
+    <Button style={{alignContent:"center", alignSelf:"center", backgroundColor:"white", color:"#022B52", width:"17rem", height:"4rem", marginTop:"1rem"}} variant="contained">No breakfast</Button>
+    
+    <a href="/lunch"><Button style={{color:"#022B52",position: 'absolute', bottom:0, right:0, marginBottom:"3rem", marginRight:"5rem", color:"white"}}>Next</Button></a>
+
     </div>
   );
 }
--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,23 +1,30 @@
 import { Typography, Button } from '@mui/material';
 
+
 const WelcomePage = () => {
   return (
-    <>
+    <div style={{marginTop:"-13rem", 
+    backgroundImage: "url(welcome-bg.png)",
+    height: "100vh",
+    width:"100%",
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'}}>
   <img
         src={`logo.svg`}
         alt="visitDenmark logo"
-        style={{width:"350px", height:"350px", marginTop:"-12rem"}}
+        style={{width:"350px", height:"350px"}}
   ></img>
   <div>
-    <Typography style={{ fontSize:"2.3rem"}}>Welcome!</Typography>
-    <Typography style={{ fontSize:"1.2rem", padding:"20px"}}>Denmark has everything you love; beautiful nature, exciting culture and wild activities. Denmark has it all 
+    <Typography style={{ fontSize:"2.3rem", color:"white"}}>Welcome!</Typography>
+    <Typography style={{ fontSize:"1.2rem", padding:"20px", color:"white"}}>Denmark has everything you love; beautiful nature, exciting culture and wild activities. Denmark has it all 
     - the hard part is choosing what to experience. Plan out your day in Aarhurs here!
     </Typography>
   </div>
 
-  <Button style={{marginTop:"4rem", backgroundColor:"#E70013", color:"white"}} variant="contained">Plan your day</Button>
+  <a href="/breakfast"><Button style={{marginTop:"4rem", backgroundColor:"#E70013", color:"white"}} variant="contained">Plan your day</Button></a>
 
-    </>
+    </div>
 
   )};
 export default WelcomePage;
