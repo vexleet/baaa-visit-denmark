15457369c6d78e35fa00a5ec458a8435a5d1e611
DumitritaPoiata <eaadupo@students.eaaa.dk>
2022-11-10 15:11:05 +0100

    Update Breakfast Buttons

--- src/pages/Breakfast.jsx
+++ src/pages/Breakfast.jsx
@@ -1,11 +1,12 @@
 import { Typography, Button } from '@mui/material';
 import React, { useState } from 'react';
 
-function Breakfast() {
-  const [active, setActive] = useState(false);
+export default function Breakfast() {
+  const [isActive, setIsActive] = useState(false);
   const handleClick = () => {
-    setActive(!active);
+    setIsActive(current => !current);
   };
+
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', 
@@ -23,41 +24,34 @@ function Breakfast() {
       <Button
         onClick={handleClick}
         style={{
-          backgroundColor: active ? '#e70013' : 'white',
-
+          backgroundColor: isActive ? '#e70013' : 'white',
+          color: isActive ? 'white' :'black',
           alignContent: 'center',
           alignSelf: 'center',
-          backgroundColor: 'white',
-          color: '#022B52',
           width: '17rem',
           height: '4rem',
           marginTop: '2rem'
-        }}
-        variant="contained">
+        }}>
         I love early breakfast
       </Button>
 
       <Button
+      onClick={handleClick}
         style={{
+          backgroundColor: isActive ? '#e70013' : 'white',
+          color: isActive ? 'white' :'black',
           alignContent: 'center',
           alignSelf: 'center',
-          backgroundColor: 'white',
-          color: '#022B52',
           width: '17rem',
           height: '4rem',
           marginTop: '1.5rem'
-        }}
-        variant="contained">
+        }}>
         I prefer brunch
       </Button>
 
-      <Typography style={{ alignContent: 'center', alignSelf: 'center', marginTop: '1rem' }}>
-        or
-      </Typography>
-
     <Typography style={{alignContent:"center", alignSelf:"center", marginTop:"1rem", color:"white"}}>or</Typography>
 
-    <Button style={{alignContent:"center", alignSelf:"center", backgroundColor:"white", color:"#022B52", width:"17rem", height:"4rem", marginTop:"1rem"}} variant="contained">No breakfast</Button>
+    <Button onClick={handleClick} style={{backgroundColor: isActive ? '#e70013' : 'white', color: isActive ? 'white' :'black', alignContent:"center", alignSelf:"center", width:"17rem", height:"4rem", marginTop:"1rem"}} >No breakfast</Button>
     
     <a href="/lunch"><Button style={{color:"#022B52",position: 'absolute', bottom:0, right:0, marginBottom:"3rem", marginRight:"5rem", color:"white"}}>Next</Button></a>
 
@@ -65,4 +59,3 @@ function Breakfast() {
   );
 }
 
-export default Breakfast;
